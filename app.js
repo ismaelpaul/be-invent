801268d52266203679bd772cf9763858e8217737
ismaelpaul <ismaafp@gmail.com>
2022-11-27 10:23:39 +0000

    Delete body parser

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const apiRouter = require('./routes/api.router');
 
 const app = express();
+
+app.use(express.json());
+
+// Route
+app.use('/api', apiRouter);
+
+// Error
+app.use('/*', (req, res) => {
+	res.status(404).send({ msg: 'Page not found.' });
+});
 
 module.exports = app;
