333497b75540195aa08a9d18c4ae008f9392ce6d
ismaelpaul <ismaafp@gmail.com>
2022-11-28 14:19:02 +0000

    Add user registration and token generation

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,5 +1,10 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/user.models');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+};
 
 exports.registerUser = asyncHandler(async (req, res) => {
 	const { name, email, password } = req.body;
@@ -23,16 +28,20 @@ exports.registerUser = asyncHandler(async (req, res) => {
 		throw new Error('Email has already been registered');
 	}
 
+	// Create new user
 	const user = await User.create({
 		name,
 		email,
 		password,
 	});
 
+	// Generate token
+	const token = generateToken(user._id);
+
 	if (user) {
 		const { _id, name, email, picture, phone, bio } = user;
 
-		res.status(201).json({ _id, name, email, picture, phone, bio });
+		res.status(201).json({ _id, name, email, picture, phone, bio, token });
 	} else {
 		res.status(400);
 		throw new Error('Invalid user data');
--- models/user.models.js
+++ models/user.models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
 	{
@@ -20,7 +21,6 @@ const userSchema = mongoose.Schema(
 			type: String,
 			required: [true, 'Please, add a password'],
 			minLength: [6, 'Password must have at least 6 characters'],
-			maxLength: [23, 'Password must not have more than 23 characters'],
 		},
 		picture: {
 			type: String,
@@ -41,6 +41,18 @@ const userSchema = mongoose.Schema(
 		timestamps: true,
 	}
 );
+
+// Encrypt password
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		return next();
+	}
+	// Hash password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(this.password, salt);
+	this.password = hashedPassword;
+	next();
+});
 
 const User = mongoose.model('User', userSchema);
 
