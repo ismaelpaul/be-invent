00e49a1ef289cf856b99e64b58dbc37a6cc6088e
ismaelpaul <ismaafp@gmail.com>
2022-12-02 09:48:32 +0000

    Add create product route and controller

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const Product = require('../models/product.models');
+const { fileSizeFormatter } = require('../utils/fileUpload');
+const cloudinary = require('cloudinary').v2;
 
 exports.createProduct = asyncHandler(async (req, res) => {
 	const { name, sku, category, quantity, price, description } = req.body;
@@ -7,6 +9,28 @@ exports.createProduct = asyncHandler(async (req, res) => {
 	if (!name || !category || !quantity || !price || !description) {
 		res.status(400);
 		throw new Error('Please fill in all fields');
+	}
+
+	let fileData = {};
+
+	if (req.file) {
+		let uploadedFile;
+		try {
+			uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+				folder: 'Invetory App',
+				resource_type: 'image',
+			});
+		} catch (error) {
+			res.status(500);
+			throw new Error('Image could not be uploaded');
+		}
+
+		fileData = {
+			fileName: req.file.originalname,
+			filePath: uploadedFile.secure_url,
+			fileType: req.file.mimetype,
+			fileSize: fileSizeFormatter(req.file.size, 2),
+		};
 	}
 
 	const product = await Product.create({
@@ -17,6 +41,7 @@ exports.createProduct = asyncHandler(async (req, res) => {
 		quantity,
 		price,
 		description,
+		image: fileData,
 	});
 
 	res.status(201).json(product);
--- routes/product.router.js
+++ routes/product.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createProduct } = require('../controllers/product.controllers');
 const { protect } = require('../middleware/auth.middleware');
+const { upload } = require('../utils/fileUpload');
 
 const productRouter = express.Router();
 
-productRouter.post('/', protect, createProduct);
+productRouter.post('/', protect, upload.single('image'), createProduct);
 
 module.exports = productRouter;
